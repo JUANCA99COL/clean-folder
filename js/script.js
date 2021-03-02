cd85c04f2c2b390890ffe40491c7d3aabed4268b
Juan Camilo CARO RODRIGUEZ <270024041@bun2-206-07.acg.ac.nz>
2021-03-02 16:16:01 +1300

    first commit for slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('hello');
+
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+})
+
+var myCarousel = document.getElementById('myCarousel')
+
+myCarousel.addEventListener('slide.bs.carousel', function () {
+  // do something...
+})
